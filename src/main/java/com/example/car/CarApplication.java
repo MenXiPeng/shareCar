0183ef98b8b610b66ce89c342769377a276dd2f9
menxipeng <menxipeng@gmail.com>
2022-09-29 23:42:42 +0800

    first commit

---
+++ src/main/java/com/example/car/CarApplication.java
@@ -0,0 +1,16 @@
+package com.example.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class CarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarApplication.class, args);
+    }
+
+}
